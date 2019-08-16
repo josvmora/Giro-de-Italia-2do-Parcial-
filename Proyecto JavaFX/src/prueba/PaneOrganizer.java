bfc55ef988feb53fbe04f9c398ff46f9e041f9be
Daniel Viscarra <>
2019-08-16 13:11:00 -0500

    Actualizacion

--- Proyecto JavaFX/src/prueba/PaneOrganizer.java
+++ Proyecto JavaFX/src/prueba/PaneOrganizer.java
@@ -9,6 +9,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -16,6 +18,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
@@ -27,6 +30,7 @@ public class PaneOrganizer {
 
     private Label lb1;
     private Label lb2;
+    private TextField ruta;
     private TextField nombre;
     private GridPane root;
     private Button boton_guardar;
@@ -45,20 +49,44 @@ public class PaneOrganizer {
         lb1 = new Label("Nombre:");
         lb2 = new Label("Imagen:");
         nombre = new TextField();
+        ruta = new TextField();
         nombre.setPromptText("Ingrese su nombre: ");
         boton_guardar = new Button("Guardar");
         file_chooser = new Button("...");
         root.add(lb1,0,0);
         root.add(lb2,0,1);
         root.add(nombre,1,0);
-        root.add(file_chooser,1,1);
-        root.add(boton_guardar,1,3,2,1);
+        root.add(ruta,1,1);
+        root.add(file_chooser,2,1);
+        root.add(boton_guardar,1,2,2,1);
         boton_guardar.setAlignment(Pos.CENTER);
         root.setHgap(10);
         root.setVgap(10);
         root.setAlignment(Pos.CENTER);
         root.setPadding(new Insets(20,20,20,20));
-        
+        file_chooser.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser fc = new FileChooser();
+                File selecionar_file = fc.showOpenDialog(null);
+                if( selecionar_file != null){
+                    ruta.setText(selecionar_file.getAbsolutePath());
+                }else{
+                    System.out.println("El archivo no se encuentra");
+                }
+                
+                
+//                PaneOrganizer.this.lb1.setText(null);
+//                PaneOrganizer.this.lb2.setText(null);
+//                PaneOrganizer.this.lbr.setText(null);
+//                PaneOrganizer.this.ganador = null;
+//                PaneOrganizer.this.btInicio.setDisable(true);
+//                
+//                new Jugador(PaneOrganizer.this,lb1).start();
+//                new Jugador(PaneOrganizer.this,lb2).start();                
+            }
+        });
         
 
     }
