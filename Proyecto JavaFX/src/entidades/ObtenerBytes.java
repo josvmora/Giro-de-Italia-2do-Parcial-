234d1b593f627274bd4409c5a199aa8e24d478e1
Jhon Espinoza <>
2019-09-01 13:14:10 -0500

    Modificacion en metodo extractBytes y creacion de metodo de creacion de imagénes

--- Proyecto JavaFX/src/entidades/ObtenerBytes.java
+++ Proyecto JavaFX/src/entidades/ObtenerBytes.java
@@ -5,24 +5,49 @@
  */
 package entidades;
 
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.io.ByteArrayOutputStream;
+import javafx.scene.image.Image;
 
 /**
  *
  * @author User
  */
 public class ObtenerBytes {
-     public byte[] extractBytes(String imgPath) throws IOException{
+
+    public byte[] extractBytes(String imgPath) throws IOException {
         File objetofile = new File(imgPath);
-        BufferedImage bufferedImage = ImageIO.read(objetofile);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(1000);
+        BufferedImage img = ImageIO.read(objetofile);
+        ImageIO.write(img, "jpg", baos);
+        //baos.flush();
+        byte[] data = baos.toByteArray();
+        //String base64String = Base64.encode(baos.toByteArray());
+        //baos.close();
+
+        //byte[] bytearray = Base64.decode(base64String);
         //RecortarFotografia recorte = new RecortarFotografia(bufferedImage,100,100);
-        WritableRaster raster =  bufferedImage.getRaster();
-        DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
-        return (data.getData());
+//        WritableRaster raster =  bufferedImage.getRaster();
+//        DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
+        return data;
+    }
+
+    public Image createImage(byte[] data, String nombre_ruta) throws IOException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(data);
+        BufferedImage bImage2 = ImageIO.read(bis);
+        ImageIO.write(bImage2, "png", new File(nombre_ruta+".png"));
+        System.out.println("image created");
+        File file = new File(nombre_ruta+".png");
+        String path = file.getAbsolutePath();
+        Image image = new Image(path);
+        return image;
+         
     }
 }
